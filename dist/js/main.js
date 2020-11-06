4dab83e63cf3e4c5e07c5ff5d0a03e516165a2d8
ishansingh26 <ishansingh23111@gmail.com>
2020-11-06 18:31:22 +0530

    commit

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".menu-btn").click(function(){
+        
+        $(".menu").toggle(1000);
+        
+    })
+})
+function myfunction(x){
+    x.classList.toggle("fa-times");
+
+}
