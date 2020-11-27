2fb2b3c983b81ab960b3ec4b705820cb3efa310b
ishansingh26 <ishansingh23111@gmail.com>
2020-11-27 18:32:52 +0530

    changing color  darkness

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
     $(".menu-btn").click(function(){
-        
-        $(".menu").toggle(1000);
-        
+        $(".menu").slideToggle(1000); 
     })
 })
 function myfunction(x){
     x.classList.toggle("fa-times");
-
 }
